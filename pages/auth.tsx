e452fd5223804f96c3ee5545f68c919f513d7859
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-07-27 07:28:40 +0530

    register logic added

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,25 +1,53 @@
- import { useCallback, useState } from 'react';
-import { NextPageContext } from 'next';
- import { useRouter } from 'next/router';
-  
-import Input from '@/components/Input';
-
- 
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { useRouter } from "next/router";
+import { getSession, signIn } from "next-auth/react";
+import Input from "@/components/Input";
+import axios from "axios";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-
-  const [variant, setVariant] = useState('login');
+  const [variant, setVariant] = useState("login");
+  const router = useRouter();
 
   const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
   }, []);
 
-  
-  
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      console.log("login completed");
+      // router.push("/profiles");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+
+      // login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-screen lg:bg-opacity-50">
@@ -29,16 +57,16 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === 'login' ? 'Sign in' : 'Register'}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
-              {variant === 'register' && (
+              {variant === "register" && (
                 <Input
                   id="name"
                   type="text"
                   label="Username"
                   value={name}
-                  onChange={(e: any) => setName(e.target.value)} 
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
               <Input
@@ -46,23 +74,31 @@ const Auth = () => {
                 type="email"
                 label="Email address or phone number"
                 value={email}
-                onChange={(e: any) => setEmail(e.target.value)} 
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                type="password" 
-                id="password" 
-                label="Password" 
+                type="password"
+                id="password"
+                label="Password"
                 value={password}
-                onChange={(e: any) => setPassword(e.target.value)} 
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button onClick={toggleVariant} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === 'login' ? 'Login' : 'Sign up'}
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
-              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
               .
             </p>
@@ -71,6 +107,6 @@ const Auth = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Auth;
